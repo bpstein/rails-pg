0f15b1c93dc67d8566940fc226e7f0e42efa89d4
Ben Stein <bumstain@gmail.com>
2014-11-13 21:53:21 +0000

    Cleaned up duplicated migrations

--- db/migrate/20141111233851_add_place_to_listings.rb
+++
@@ -1,5 +0,0 @@
-class AddPlaceToListings < ActiveRecord::Migration
-  def change
-  	add_column :listings, :place, :string
-  end
-end
--- db/migrate/20141112014603_add_location_to_listing.rb
+++
@@ -1,8 +0,0 @@
-class AddLocationToListing < ActiveRecord::Migration
-  def change
-    add_column :listings, :address, :string
-    add_column :listings, :city, :string
-    add_column :listings, :postcode, :string
-    add_column :listings, :country, :string
-  end
-end
